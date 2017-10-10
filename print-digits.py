de609275873480031573754338aa7175eb3dcf57
vagrant <vagrant@vagrant.vm>
2017-10-10 22:28:03 +0000

    solved problem

--- print-digits.py
+++ print-digits.py
@@ -22,6 +22,16 @@ For example::
 def print_digits(num):
     """Given int, print digits in reverse order, starting with the ones place."""
 
+    # CAN'T USE STRING MANIPULATION; USE MATH
+    # need to figure out how to strip 413 to its ones place and print that
+    # modulo involved? dividing by 1/10/100 will give remainder of ones place
+    # loop through num, stripping 413 --> 41 --> 4
+
+    while num:
+        next_digit = num % 10
+        print next_digit
+        num = (num - next_digit) / 10
+
 
 if __name__ == '__main__':
     import doctest
